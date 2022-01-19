d2a40fd6694e1733d101e6831d4beae8f482b2b2
Mauricio Villa <vbamauricio@gmail.com>
2022-01-19 13:02:30 -0300

    Mejorada escritura de routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :prospectos, except: %i[index] do
+  resources :prospectos, except: :index do
     resources :conversations, only: :create
   end
   resources :conversations, only: :destroy
-  devise_for :users, path: '', except: %i[create]
+  devise_for :users, skip: [:registrations], path: ''
   root to: 'prospectos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
